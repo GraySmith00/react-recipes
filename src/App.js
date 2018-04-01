c8a43c98d518ff1daf7468c7dfc99775e5b7528d
Gray Smith <gray.smith00@gmail.com>
2018-04-01 17:41:57 +0000

    add recipe input form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import Recipe from './Recipe';
 import Navbar from './Navbar';
+import RecipeInput from './RecipeInput';
 import RecipeList from './RecipeList';
 import './App.css';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipes: [
+        {
+          id: 1,
+          title: 'Prince Pizza',
+          instructions: 'Iceland venmo celiac pinterest disrupt freegan asymmetrical banh mi 8-bit tbh viral.',
+          ingredients: ['bread', 'tomatoe sauce', 'pepperoni'],
+          img: 'princePizza.jpg'
+        },
+        {
+          id: 2,
+          title: 'Veggie Taco',
+          instructions: 'Pinterest shoreditch echo park, shabby chic actually chia tacos meh tumeric lomo iceland offal knausgaard.',
+          ingredients: ['hummus', 'peppers', 'sweet potato'],
+          img: 'veggieTaco.jpg'
+        },
+        {
+          id: 3,
+          title: 'Roasted Vegetables',
+          instructions: 'Yr echo park direct trade neutra artisan hot chicken next level vaporware meh.',
+          ingredients: ['potatoes', 'red onion', 'cauliflower'],
+          img: 'roastedVeggies.jpg'
+        }      
+      ],
+      nextRecipeId: 4
+    };
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeList />
+        <RecipeInput />
+        <RecipeList recipes={this.state.recipes}/>
       </div>
     );
   }
