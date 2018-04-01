42f1f046a8da6e2d0187f911aabee94f7c8b030c
Gray Smith <gray.smith00@gmail.com>
2018-04-01 17:53:50 +0000

    add recipe save functionality

--- src/App.js
+++ src/App.js
@@ -12,35 +12,48 @@ class App extends Component {
     this.state = {
       recipes: [
         {
-          id: 1,
+          id: 0,
           title: 'Prince Pizza',
           instructions: 'Iceland venmo celiac pinterest disrupt freegan asymmetrical banh mi 8-bit tbh viral.',
           ingredients: ['bread', 'tomatoe sauce', 'pepperoni'],
           img: 'princePizza.jpg'
         },
         {
-          id: 2,
+          id: 1,
           title: 'Veggie Taco',
           instructions: 'Pinterest shoreditch echo park, shabby chic actually chia tacos meh tumeric lomo iceland offal knausgaard.',
           ingredients: ['hummus', 'peppers', 'sweet potato'],
           img: 'veggieTaco.jpg'
         },
         {
-          id: 3,
+          id: 2,
           title: 'Roasted Vegetables',
           instructions: 'Yr echo park direct trade neutra artisan hot chicken next level vaporware meh.',
           ingredients: ['potatoes', 'red onion', 'cauliflower'],
           img: 'roastedVeggies.jpg'
         }      
       ],
-      nextRecipeId: 4
+      nextRecipeId: 3
     };
+    
+    this.handleSave = this.handleSave.bind(this);
   }
+  
+  handleSave(recipe) {
+    this.setState((prevState, props) => {
+      const newRecipe = {...recipe, id: this.state.nextRecipeId};
+      return {
+        nextRecipeId: prevState.nextRecipeId + 1,
+        recipes: [...this.state.recipes, newRecipe]
+      }
+    })
+  }
+  
   render() {
     return (
       <div className="App">
         <Navbar />
-        <RecipeInput />
+        <RecipeInput onSave={this.handleSave}/>
         <RecipeList recipes={this.state.recipes}/>
       </div>
     );
